54a477d7391a165c8db1f41d630491411fb63032
D0770799 <ip1259@gmail.com>
2018-10-09 13:51:29 +0800

    finish the class
    增加屬性,建構子,getter,setter,ovreride toString()

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,4 +1,49 @@
 
 public class Exercise {
+	
+	private String name = "";
+	private int score_Received = 0 ;
+	private int total_Point = 0 ;
+	
+	Exercise()
+	{
+		
+	}
+	Exercise(String str, int sR, int tP)
+	{
+		name = str;
+		score_Received = sR;
+		total_Point = tP;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	public int getScoreReceived()
+	{
+		return score_Received;
+	}
+	public int getTotalPoint()
+	{
+		return total_Point;
+	}
+	public void setName(String str)
+	{
+		name = str;
+	}
+	public void setScoreReceived(int n)
+	{
+		score_Received = n;
+	}
+	public void setTotalPoint(int n)
+	{
+		total_Point = n;
+	}
+	
+	public String toString() 
+	{
+		return String.format("%-30s%-10d%-10d", name, score_Received, total_Point);
+	}
 
 }
